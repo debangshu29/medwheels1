af5a7b5126e442181b900f26edf8c188ed2ec1f0
debangshu29 <126980330+debangshu29@users.noreply.github.com>
2025-09-18 22:36:04 +0530

    Update asgi.py

--- medwheels/asgi.py
+++ medwheels/asgi.py
@@ -1,16 +1,17 @@
 import os
 
-# MUST set the DJANGO_SETTINGS_MODULE before importing Django modules
+# MUST set the DJANGO_SETTINGS_MODULE before anything else that touches Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medwheels.settings')
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
-# Now safe to import your app routing/consumers
-import main.routing
-
+# Call get_asgi_application() now to run django.setup() so apps are ready
 django_asgi_app = get_asgi_application()
+
+# Now it's safe to import your app routing which may import models/consumers
+import main.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
