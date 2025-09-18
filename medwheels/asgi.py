1f73fa470c3feb60790fe1888f61ce402a7511da
debangshu29 <126980330+debangshu29@users.noreply.github.com>
2025-09-18 22:03:43 +0530

    Update asgi.py

--- medwheels/asgi.py
+++ medwheels/asgi.py
@@ -1,10 +1,8 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-
-# Import your project's websocket routing from the MAIN app
-# Change `main.routing` if your routing file is at a different module.
-import main.routing
+from channels.auth import AuthMiddlewareStack
+import main.routing  # your websocket URL patterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medwheels.settings')
 
@@ -12,7 +10,9 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": URLRouter(
-        main.routing.websocket_urlpatterns
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            main.routing.websocket_urlpatterns
+        )
     ),
 })
